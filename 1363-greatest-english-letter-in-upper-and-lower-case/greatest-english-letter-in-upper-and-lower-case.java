70e41fec59b442b9a6be3c1caecc4b255fd87ba0
BOOPATHI U <boopathiboo142@gmail.com>
2026-09-15 11:39:06 +0530

    Time: 4 ms (68.85%) | Memory: 43.3 MB (71.89%) - LeetSync

---
+++ 1363-greatest-english-letter-in-upper-and-lower-case/greatest-english-letter-in-upper-and-lower-case.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String greatestLetter(String s) {
+
+        boolean[] lower = new boolean[26];
+        boolean[] upper = new boolean[26];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                lower[ch - 'a'] = true;
+            } else {
+                upper[ch - 'A'] = true;
+            }
+        }
+
+        for (int i = 25; i >= 0; i--) {
+
+            if (lower[i] && upper[i]) {
+                return String.valueOf((char)('A' + i));
+            }
+        }
+
+        return "";
+    }
+}
